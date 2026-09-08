17b2ddc8b5c5d2fa210d5767dae850fcdd2416f9
anurag-kumar <kumaranurag.7278@gmail.com>
2026-09-08 19:10:10 +0530

    loop through padas DF

--- comprehension/dict_comprehension.py
+++ comprehension/dict_comprehension.py
@@ -10,6 +10,24 @@ import random
 names = ["Alex","Beth","caroline","Dave","Elanor","Freddie"]
 
 student_scores = {student:random.randint(1,100) for student in names}
-print(student_scores)
+# print(student_scores)
 passed_studemts = {student:score for (student,score) in student_scores.items() if score >= 60 }
-print(passed_studemts)
+# print(passed_studemts)
+
+
+# ITERATE OVER PANDA DATAFRAME
+student_dict = {
+    "student": ["Angela","James","Lily"],
+    "score":[56,78,89]
+}
+
+import pandas as pd
+
+student_data_frame = pd.DataFrame(student_dict)
+# print(student_data_frame)
+
+# Loop through rows in data frame 
+
+for (index,row ) in student_data_frame.iterrows():
+    # print(index)
+    print(row.student)
