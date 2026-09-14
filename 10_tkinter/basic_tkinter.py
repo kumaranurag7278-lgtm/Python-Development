41df1e35f39eaf4ac9dac673d79881c59c78256e
anurag-kumar <kumaranurag.7278@gmail.com>
2026-09-14 18:42:31 +0530

    lean button and entry

--- 10_tkinter/basic_tkinter.py
+++ 10_tkinter/basic_tkinter.py
@@ -1,17 +1,34 @@
-import tkinter
+from tkinter import *
 
 # create a window :
-window = tkinter.Tk()
+window = Tk()
 window.title("my first GUI program")
 
 # size of the window 
 window.minsize(width=500,height=300)
 
 # Label :
-my_label = tkinter.Label(text="I am a Label",font=("Arial",24,"italic"))
+my_label = Label(text="I am a Label",font=("Arial",24,"italic"))
 my_label.pack(side="top")
 
 
+# change text 
+# my_label.config(text="New Text") #another way of changing the text
 
+
+# Button
+def button_clicked():
+    # my_label["text"] = "onichan came inside me"
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+    print("onichan yamate kudasai")
+
+button = Button(text="Click me daddy",command = button_clicked)
+button.pack()
+
+# Entry 
+input = Entry(width=15)
+input.pack()
 
 window.mainloop()
