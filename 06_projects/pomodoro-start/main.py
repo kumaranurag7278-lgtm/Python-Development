50c70cdfeefa47869b44bddb98de2a2b8880be0e
anurag-kumar <kumaranurag.7278@gmail.com>
2026-09-15 22:49:01 +0530

    Added time reset fucntion and break timer also fixed some ui glitch

--- 06_projects/pomodoro-start/main.py
+++ 06_projects/pomodoro-start/main.py
@@ -9,21 +9,57 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN *60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60 
+    if reps % 8 == 0 :
+        count_down(long_break_sec)
+        title_label.config(text="Short Break",fg=RED)
+
+    elif reps % 2  == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Short Break",fg=PINK)
+
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work Time",fg=GREEN)
+
+
+
+
     count_down(WORK_MIN*60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
 
     count_min = math.floor(count/60)
     count_sec = count % 60
+# fixing the seconds display when it goes to less than 10:
+    if count_sec < 10 :
+        count_sec = F"0{count_sec}"
+
+# time countdown  disply 
 
     canvas.itemconfig(timer_text,text= f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000,count_down,count-1)
+    else :
+        start_timer()
+        mark = " "
+        
+        for _ in range(math.floor(reps/2)):
+            mark += "✓"
+        check_mark.config(text=mark)
+        
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -39,18 +75,16 @@ canvas.create_image(100,112,image = tomato_img)
 timer_text = canvas.create_text(100,130, text= "00:00",fill = "white",font=(FONT_NAME,20,"bold"))
 canvas.grid(column=2,row=2)
 
-#calling countdown
-# count_down(50)
 
 # Timer label 
-timer = Label(text="Timer",fg=GREEN,font=(FONT_NAME,50),bg=YELLOW)
-timer.grid(column=2,row=0)
+title_label = Label(text="Timer",fg=GREEN,font=(FONT_NAME,50),bg=YELLOW)
+title_label.grid(column=2,row=0)
 
 
 # checkmark
 
-mark = Label(text="✓",fg=GREEN,bg=YELLOW,font=(20))
-mark.grid(column=2,row=3)
+check_mark = Label(fg=GREEN,bg=YELLOW,font=(20))
+check_mark.grid(column=2,row=3)
 
 
 # start button 
@@ -60,8 +94,6 @@ start_button.grid(column=0,row=3)
 # stop button
 reset_button = Button(width=7,text="Reset", highlightthickness=0)
 reset_button.grid(column=3,row=3)
-
-
 
 
 
